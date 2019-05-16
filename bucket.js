9013db641783bdf1d0f12a95ab086b938c6614bd
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2019-05-15 22:01:47 -0400

    some additions

--- bucket.js
+++ bucket.js
@@ -1,14 +1,30 @@
 Vue.component('bucket', {
+  prop: ['bucket'],
   template: `
   <div class="container">
     <div class="row">
       <div class="col bucket-shape">
-        Bucket
+        {{ bucket }}
       </div>
     </div>
   </div>`
 })
 
-var bucket = new Vue({
-  el: "bucket",
+Vue.component('drain', {
+  template: `
+  <div class="container">
+    <div class="row">
+      <div class="col drain-shape">
+        {{ drain }}
+      </div>
+    </div>
+  </div>`
+})
+
+var bucketComplex = new Vue({
+  el: "#bucket-complex",
+  data: {
+    bucket: "Bucket it",
+    drain: "Drain it"
+  }
 })
