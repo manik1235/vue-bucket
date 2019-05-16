227fbb556902c0c2e0115fe9d789ec8a93eb0a41
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-05-16 00:08:26 -0400

    Add prototype drain
    In order to get started with adding the drain components, this commit
    adds the `#draw` method to the `drain` component.
    
    This commit draws two drains and one bucket.

--- bucket.js
+++ bucket.js
@@ -1,5 +1,9 @@
 Vue.component('bucket', {
-  prop: ['bucket'],
+  data: function () {
+    return {
+      bucket: "Bucket"
+    }
+  },
   template: `
   <div class="container">
     <div class="row">
@@ -11,11 +15,36 @@ Vue.component('bucket', {
 })
 
 Vue.component('drain', {
+  props: ['id', 'title'],
+  data: function () {
+    return {
+      drain: "Drain"
+    }
+  },
+  methods: {
+    draw: function () {
+      $( document ).ready( function () {
+        var $canvasses = $( '.drain-canvas' );
+        $canvasses.each( function ( index, canvas ) {
+          if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            ctx.moveTo(0, 0);
+            ctx.lineTo(200, 100);
+            ctx.stroke();
+          }
+        })
+      })
+    }
+  },
   template: `
   <div class="container">
     <div class="row">
       <div class="col drain-shape">
-        {{ drain }}
+        <h3>{{ id }}: {{ title }}</h3>
+        <canvas v-bind:id="'drain-' + id" class="drain-canvas">
+          <!-- I could render an html partial if the browser doesn't support canvas -->
+        </canvas>
+        {{ draw() }}
       </div>
     </div>
   </div>`
@@ -24,7 +53,9 @@ Vue.component('drain', {
 var bucketComplex = new Vue({
   el: "#bucket-complex",
   data: {
-    bucket: "Bucket it",
-    drain: "Drain it"
+    drains: [
+      { id: 1, title: 'I give to other' },
+      { id: 2, title: 'Other takes from me' }
+    ]
   }
 })
